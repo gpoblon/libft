d5e0237d0ca5646c59d7fb37933f77ddbba03996
Gwojda <gwojda@student.42.fr>
2018-03-14 11:02:12 +0100

    Update ft_gnl.c

--- src/ft_gnl.c
+++ src/ft_gnl.c
@@ -10,67 +10,112 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "get_next_line.h"
 
-static int		ft_check_leftover(char **leftover, char **line, int i)
+static int	ft_check_buff_2(char **line, char **content, int i)
 {
-	if (*leftover)
+	char	*dup;
+	char	*tmp2;
+
+	tmp2 = ft_strsub(*content, 0, i);
+	*line = ft_strjoin(*line, tmp2);
+	free(tmp2);
+	dup = ft_strdup(*content + i + 1);
+	free(*content);
+	*content = dup;
+	return (1);
+}
+
+int			ft_check_buff(char **line, char **content)
+{
+	int		i;
+	char	*tmp;
+	char	*tmp2;
+
+	if (!*content)
+		return (0);
+	i = ft_strlen(*content);
+	if ((tmp = ft_strchr(*content, '\n')))
+		return (ft_check_buff_2(line, content, tmp - *content));
+	else
 	{
-		while ((*leftover)[i] && (*leftover)[i] != '\n')
-			i++;
-		*line = ft_strfjoin(*line,
-				ft_strfsub(*leftover, 0, i, F_N), F_A);
-		if ((size_t)i < ft_strlen(*leftover))
-		{
-			*leftover = ft_strfsub(*leftover, i + 1,
-							ft_strlen(*leftover) - i, F_1);
-			return (1);
-		}
-		else
-			free(*leftover);
+		tmp2 = *line;
+		*line = ft_strjoin(*line, *content);
+		free(tmp2);
+		ft_strdel(content);
 	}
 	return (0);
 }
 
-static int		ft_read_fd(char **leftover, char **line, char *buff, int fd)
+static int	ft_reader_2(char **line, t_lst *lst, char *buff)
 {
-	int		reader;
+	char	*tmp;
+	char	*tmp2;
 	int		i;
 
-	while ((reader = read(fd, buff, BUFF_SIZE)))
+	if ((tmp = ft_strchr(buff, '\n')))
 	{
-		if (reader < 0)
-			return (-1);
-		i = 0;
-		buff[reader] = '\0';
-		while (i < reader && buff[i] != '\n')
-			i++;
-		if (buff[i] == '\n')
-		{
-			*leftover = ft_strfsub(buff, i + 1, reader - (i + 1), F_N);
-			*line = ft_strfjoin(*line, ft_strfsub(buff, 0, i, 0), F_A);
-			return (1);
-		}
-		else
-			*line = ft_strfjoin(*line, buff, F_1);
-	}
-	if (*line[0] != 0)
+		i = tmp - buff;
+		tmp2 = ft_strsub(buff, 0, i);
+		tmp = *line;
+		*line = ft_strjoin(*line, tmp2);
+		free(tmp2);
+		free(tmp);
+		lst->content = ft_strdup(buff + i + 1);
 		return (1);
-	free(*line);
+	}
+	else
+	{
+		tmp2 = *line;
+		*line = ft_strjoin(*line, buff);
+		free(tmp2);
+	}
 	return (0);
 }
 
-int				get_next_line(const int fd, char **line)
+int			ft_reader(char **line, t_lst *lst)
 {
-	static char	*leftover = NULL;
-	char		buff[BUFF_SIZE + 1];
-	int			i;
+	int		ret;
+	char	buff[BUFF_SIZE + 1];
 
-	i = 0;
-	if (!line || fd < 0)
-		return (-1);
-	*line = ft_strdup("");
-	if (ft_check_leftover(&leftover, line, i) == 1)
+	while ((ret = read(lst->fd, buff, BUFF_SIZE)))
+	{
+		if (ret == -1)
+			return (ret);
+		buff[ret] = '\0';
+		if (ft_reader_2(line, lst, (char *)buff))
+			return (1);
+	}
+	if (ret == 0 && **line == '\0')
+		return (0);
+	else
 		return (1);
-	return (ft_read_fd(&leftover, line, buff, fd));
+}
+
+int			get_next_line(int const fd, char **line)
+{
+	static t_lst	*begin_list = NULL;
+	t_lst			*lst;
+	char			tmp;
+
+	if (!line || read(fd, &tmp, 0) == -1 || fd < 0)
+		return (-1);
+	if (*line)
+		*line = NULL;
+	*line = ft_strdup("");
+	lst = begin_list;
+	while (lst && lst->fd != fd)
+		lst = lst->next;
+	if (!lst)
+	{
+		ft_lst_push_back_gnl(&begin_list, fd);
+		lst = begin_list;
+		while (lst && lst->fd != fd)
+			lst = lst->next;
+	}
+	else if (ft_check_buff(line, &(lst->content)))
+		return (1);
+	if (!begin_list)
+		begin_list = lst;
+	return (ft_reader(line, lst));
 }
