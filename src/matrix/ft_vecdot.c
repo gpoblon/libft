f0d1f5b0d978ee7a31c6d8d59afb40dfb6ead3ed
Gaetan POBLON <gpoblon@e1r5p20.42.fr>
2018-11-02 11:48:48 +0100

    array version of vectors done

--- src/matrix/ft_vecdot.c
+++ src/matrix/ft_vecdot.c
@@ -6,7 +6,7 @@
 /*   By: gpoblon <gpoblon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/30 14:10:53 by gpoblon           #+#    #+#             */
-/*   Updated: 2018/10/31 15:30:36 by gpoblon          ###   ########.fr       */
+/*   Updated: 2018/11/02 11:05:58 by gpoblon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,5 @@
 
 float	ft_vec3dot(t_vec3 a, t_vec3 b)
 {
-	t_vec3	tmp1;
-	float		t1;
-	float		t2;
-	float		t3;
-
-	ft_vec3cpy(tmp1, a);
-	ft_vec3sum(tmp1, b);
-	t1 = ft_vec3len(tmp1) * ft_vec3len(tmp1);
-	t2 = ft_vec3len(a) * ft_vec3len(a);
-	t3 = ft_vec3len(b) * ft_vec3len(b);
-	return (0.5 * (t1 - t2 - t3));
+	return (a[0] * b[0] + a[1] * b[1] + a[2] * b[2]);
 }
